8d14ab83866a9f1ae92571e5dfbc3d65af0254e8
Oystein Kjarnet <oystein.kjaernet@entur.org>
2025-05-12 14:55:53 +0200

    feat: read input from console

--- src/main/java/org/entur/automat/oppgave/IntervjuOppgaveApplication.java
+++ src/main/java/org/entur/automat/oppgave/IntervjuOppgaveApplication.java
@@ -1,9 +1,16 @@
 package org.entur.automat.oppgave;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class IntervjuOppgaveApplication {
 
-	public static void main(String[] args) {
-		System.out.println("hello world");
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter String: ");
+		String input = br.readLine();
+		System.out.println("hello world, " + input);
 	}
 
 }
